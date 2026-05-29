67c65a9b6fa77136a9db2880a575809f9bcb9672
Rishav Raj <rishav9465@gmail.com>
2026-05-29 15:42:20 +0530

    Fruits

---
+++ 34.Backend 1 (Node.js)/Fruits/index.js
@@ -0,0 +1,9 @@
+const apple = require("./apple");
+const banana = require("./banana");
+const orange = require("./orange");
+
+let fruits = [apple,banana,orange];
+module.exports = fruits;
+
+// this file name should be
+//index.js only that combines all files
