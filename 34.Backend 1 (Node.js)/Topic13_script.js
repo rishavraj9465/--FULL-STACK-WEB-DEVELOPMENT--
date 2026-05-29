cda3f81e16840f1ce7acd35506a7a2e413815c6e
Rishav Raj <rishav9465@gmail.com>
2026-05-29 15:54:13 +0530

    34.Backend 1 (Node.js)

---
+++ 34.Backend 1 (Node.js)/Topic13_script.js
@@ -0,0 +1,7 @@
+import {sum} from "./Topic13_math.js";
+console.log(sum(1,2));
+
+import { generate} from "random-words";
+console.log(generate());
+
+//"type": "module" in package .json to run this 
