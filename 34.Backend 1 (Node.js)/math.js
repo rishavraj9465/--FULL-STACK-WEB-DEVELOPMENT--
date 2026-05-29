90eb540183b491b995bf546a383c88c805ad2295
Rishav Raj <rishav9465@gmail.com>
2026-05-29 15:50:47 +0530

    34.Backend 1 (Node.js)

---
+++ 34.Backend 1 (Node.js)/math.js
@@ -0,0 +1,25 @@
+module.exports.sum = (a,b) => a+b ;
+module.exports.mul = (a,b) => a*b ;
+exports.g = 9.8 ;
+//but exports= 9.8 will give err
+ 
+module.exports.PI = 3.14 ;
+
+// let obj ={
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     PI:PI
+// }
+
+//module.exports =obj;
+
+
+// module.exports ={
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     PI:PI
+// }
+
+
